27c11ddbe1184bff3c47ed37248f5398cbb1d843
touchstone-tools <nhussaints@gmail.com>
2026-06-19 11:49:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
+const CACHE_NAME = "zip-lookup-v1";
 
+const FILES = [
+  "./",
+  "./index.html",
+  "./zipdata.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"
+];
+
+// Install
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+// Activate
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
